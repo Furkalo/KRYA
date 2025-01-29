a42c5f52c8fdea830414c94cc343d37a6e29209b
Diana Tavolzhan <di.tavolzhan@gmail.com>
2025-01-29 17:51:26 +0200

    added into Gun.py class method calculate_rotation

--- BackEnd/Gun.py
+++ BackEnd/Gun.py
@@ -1,3 +1,5 @@
+import math
+
 class Gun:
     """
      The Gun class represents a gun in a 2D game that can rotate based on the mouse position
@@ -16,4 +18,21 @@ class Gun:
         self.height = height
         self.gun_images = gun_images
         self.lasers = ['red', 'purple', 'green']
+
+    def calculate_rotation(self, mouse_pos):
+        """
+        Calculates the rotation angle required for the gun to point towards the mouse position.
+
+        :param mouse_pos:  The current position of the mouse (x, y)
+        :return: The rotation angle in degrees that the gun needs to rotate to aim at the mouse.
+        """
+        gun_point = (self.width / 2, self.height - 200)
+
+        if mouse_pos[0] != gun_point[0]:
+            slope = (mouse_pos[1] - gun_point[1]) / (mouse_pos[0] - gun_point[0])
+        else:
+            slope = -100000
+
+        angle = math.atan(slope)
+        return math.degrees(angle)
 
