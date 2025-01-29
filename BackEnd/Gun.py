1eeb96b5260de052b733f6b689a906577576a0dc
Diana Tavolzhan <di.tavolzhan@gmail.com>
2025-01-29 18:15:01 +0200

    added into Gun.py class method draw

--- BackEnd/Gun.py
+++ BackEnd/Gun.py
@@ -1,4 +1,5 @@
 import math
+import pygame
 
 class Gun:
     """
@@ -35,4 +36,36 @@ class Gun:
 
         angle = math.atan(slope)
         return math.degrees(angle)
+
+
+    def draw(self, screen, level, mouse_pos):
+        """
+        Draws the gun rotated according to the mouse position, and fires a laser if the mouse button is pressed.
+
+        :param screen: The surface to draw the gun on.
+        :param level: The current level of the game that determines the gun and laser color.
+        :param mouse_pos: The current position of the mouse (x, y).
+        :return: None
+        """
+        rotation = self.calculate_rotation(mouse_pos)
+        gun_point = (self.width / 2, self.height - 200)
+
+        if mouse_pos[0] < self.width / 2:
+            gun = pygame.transform.flip(self.gun_images[level - 1], True, False)
+            if mouse_pos[1] < 600:
+                rotated_gun = pygame.transform.rotate(gun, 90 - rotation)
+                screen.blit(rotated_gun, (self.width / 2 - 90, self.height - 250))
+
+                clicks = pygame.mouse.get_pressed()
+                if clicks[0]:
+                    pygame.draw.circle(screen, self.lasers[level - 1], mouse_pos, 5)
+        else:
+            gun = self.gun_images[level - 1]
+            if mouse_pos[1] < 600:
+                rotated_gun = pygame.transform.rotate(gun, 270 - rotation)
+                screen.blit(rotated_gun, (self.width / 2 - 30, self.height - 250))
+
+                clicks = pygame.mouse.get_pressed()
+                if clicks[0]:
+                    pygame.draw.circle(screen, self.lasers[level - 1], mouse_pos, 5)
 
